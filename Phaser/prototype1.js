def19adf401b6a798ed2ee39e3548ac80ad07ce4
valexis22009 <valexis22009@tamu.edu>
2017-02-09 08:41:50 -0600

    Updated game

--- Phaser/prototype1.js
+++ Phaser/prototype1.js
@@ -1,61 +1,85 @@
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
+var windowWidth = 500;//window.innerWidth;
+var windowHeight = 500;//window.innerHeight;
 
 var game = new Phaser.Game(windowWidth, windowHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 var obstacles1;
 var obstacles2;
-var mummy;
+var player;
+var score = 0;
+var scoreLabel;
+var frameCount = 0;
 
 
 function preload() {
 
-     game.load.spritesheet('mummy', 'assets/metalslug_mummy37x45.png', 37, 45, 18);
-	 game.load.image('obstacle', 'assets/obstacle.png');
+     game.load.image('player', 'assets/droplet.png');
+	 game.load.image('obstacle', 'assets/oil.png');
 }
 
 function create(){
+	game.stage.backgroundColor = '#71c5cf';
+	scoreLabel = game.add.text(20, 20, "0");
+	console.log(windowHeight / 4);
 	
-	mummy = game.add.sprite(10, windowHeight * 3 / 4, 'mummy');
-	var walk = mummy.animations.add('walk');
-	mummy.animations.play('walk', 30, true);
+	
+	player = game.add.sprite(10, windowHeight * 3 / 4, 'player');
+	game.physics.arcade.enable(player);
+	
 	
 	obstacles1 = game.add.group();
-	obstacles1.enableBody = true;
-	obstacles1.physicsBodyType = Phaser.Physics.ARCADE;
+	game.physics.arcade.enable(obstacles1);
+	var c = game.add.sprite(windowWidth, windowHeight/4, 'obstacle');
+	game.physics.arcade.enable(c);
+	c.body.velocity.x = -100;
+	obstacles1.add(c);
 	
 	obstacles2 = game.add.group();
-	obstacles2.enableBody = true;
-	obstacles2.physicsBodyType = Phaser.Physics.ARCADE;
+	game.physics.arcade.enable(obstacles2);
+	c = game.add.sprite(windowWidth + 300, windowHeight * 3 / 4, 'obstacle');
+	game.physics.arcade.enable(c);
+	c.body.velocity.x = -100;
+	obstacles2.add(c);
 	
-	for (var i = 0; i < 5; ++i){
-		var c = obstacles1.create(game.world.randomX + 100, windowHeight/4, 'obstacle', game.rnd.integerInRange(0, 36));
-		var w = c.animations.add('walk');
-		c.animations.play('walk', 30, true);
+	var yAxis = windowHeight * 3 / 4;
+	for (var i = 0; i < 10; ++i){
+		if (game.rnd.integerInRange(0,1) === 0){
+			yAxis = windowHeight * 3 / 4;
+		}
+		else yAxis = windowHeight/4;
+		var c = game.add.sprite(obstacles1.getAt(i).x + game.rnd.integerInRange(100, 500), yAxis, 'obstacle');
+		game.physics.arcade.enable(c);
 		c.body.velocity.x = -100;
+		obstacles1.add(c);
 	}
 	
-	for (var i = 0; i < 5; ++i){
-		var c = obstacles2.create(game.world.randomX + 100, windowHeight * 3 / 4, 'obstacle', game.rnd.integerInRange(0, 36));
-		var w = c.animations.add('walk');
-		c.animations.play('walk', 30, true);
-		c.body.velocity.x = -100;
-	}
-	
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.physics.arcade.enable([mummy, obstacles1, obstacles2]);
 }
 
 function update(){
 	
+	if (player.body.y > windowHeight * 3 / 4){
+		player.body.velocity.y = 0
+	}
+	
+	if (player.body.y < windowHeight / 4){
+		player.body.velocity.y = 0
+	}
+	
+	console.log(player.body.y);
+	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-		mummy.y = windowHeight/4;
+		player.body.velocity.y = -200;
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-		mummy.y = windowHeight * 3 / 4;
+		player.body.velocity.y = 200;
 	}
 	
-	game.physics.arcade.overlap(mummy, [obstacles1, obstacles2], hit, null, this);
+	game.physics.arcade.collide(player, [obstacles1, obstacles2], hit, null, this);
+	frameCount++;
+	if (frameCount % 60 === 0){
+		score++;
+		scoreLabel.text = score;
+	}
 }
 
 function hit(){
