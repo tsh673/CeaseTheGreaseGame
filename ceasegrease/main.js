0d1180b62af4b14e3fecfd060e245e9b0271165e
Chance Eckert <cwe1994@tamu.edu>
2017-04-25 10:33:47 -0500

    formatting
    formatting and minor syntax.

--- ceasegrease/main.js
+++ ceasegrease/main.js
@@ -224,7 +224,7 @@ var gameOverState = {
             game.state.start('score');
         }, self); // Input listener go to score screen on mouse click
 
-        
+
     },
     update: function ()
     {
@@ -505,13 +505,12 @@ var scoreState = {
                 letter = "M.";
             }, this, 2, 1, 0); // Keyboard button
             var entButton = game.add.button(143, game.world.centerY + 180, 'enter', function () {
-				if (word == ""){
-					game.state.start('leader');
-				}
-                else {
-					localStorage.setItem(score.toString(), word);
-					game.state.start('leader');
-				}
+                if (word === "") {
+                    game.state.start('leader');
+                } else {
+                    localStorage.setItem(score.toString(), word);
+                    game.state.start('leader');
+                }
             }, this, 2, 1, 0); // Enter button
         }
     },
